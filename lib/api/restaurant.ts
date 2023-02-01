2aff3c71b8f54b3ad3ef9eaaa7eb02aee2caa5e2
cedric654 <gagneced22@hotmail.com>
2023-02-01 11:03:04 -0500

    Update restaurant API

--- lib/api/restaurant.ts
+++ lib/api/restaurant.ts
@@ -1,31 +1,64 @@
 import { db } from "@/lib/firebase";
-import { doc, getDoc, getDocs, limit, query, where} from "firebase/firestore";
+import {
+  doc,
+  getDoc,
+  getDocs,
+  limit,
+  query,
+  setDoc,
+  where,
+} from "firebase/firestore";
 import IRestaurant from "@/interfaces/IRestaurant";
 
+export const addRestaurant = async () => {
+  // restaurant info needed
+  // try {
+  //   const restaurantRef = await(db.restaurants, // id)
+  //   await setDoc(restaurantRef)
+  // } catch (error) {
+  // }
+};
+
 export const getAllRestaurants = async () => {
-  const userDoc = await getDocs(db.restaurants);
-  console.log(userDoc.docs);
-  let Restaurants: IRestaurant[] = [];
-  userDoc.forEach((doc) => {
-    Restaurants.push(doc.data());
-  });
-  console.log(Restaurants)
-  return Restaurants
+  try {
+    const querySnapshot = await getDocs(db.restaurants);
+    let restaurants: IRestaurant[] = [];
+    querySnapshot.forEach((doc) => {
+      restaurants.push(doc.data());
+    });
+    return restaurants;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-export const getRestaurantById = async () => {
-  const docRef = doc(db.restaurants, "TEST");
-  const docSnap = await getDoc(docRef);
-  return docSnap
+export const getRestaurantById = async (id: string) => {
+  try {
+    const docRef = doc(db.restaurants, id);
+    const restaurant = await getDoc(docRef);
+    return restaurant;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-export const getRestaurantByName = async (name : string) => {
-  const q = query(
-    db.restaurants,
-    where("name", "==", name),
-    limit(1)
-  );
-  const querySnapshot = await getDocs(q);
-  console.log(querySnapshot.docs[0].data() )
-  return querySnapshot.docs[0] 
+export const getRestaurantByName = async (name: string) => {
+  try {
+    const q = query(db.restaurants, where("name", "==", name), limit(1));
+    const querySnapshot = await getDocs(q);
+    console.log(querySnapshot.docs[0].data());
+    return querySnapshot.docs[0];
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const editRestaurant = async (id: string) => {
+  try {
+  } catch (error) {}
+};
+
+export const deleteRestaurant = async (id: string) => {
+  try {
+  } catch (error) {}
 };
